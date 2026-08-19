67e8bbecf5487e8bfb78bf49f8cae38c0963b4c2
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-08-19 20:59:09 +0530

    Add model

---
+++ BlogPost_Models/Data/DTOs/BlogImageDTO/CreateBlogImageDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost_Models.Data.DTOs.BlogImageDTO
+{
+    public class CreateBlogImageDTO
+    {
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
