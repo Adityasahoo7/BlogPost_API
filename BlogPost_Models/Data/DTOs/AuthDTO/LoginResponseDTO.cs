924aa2359f21dfcbe1ce66b3d0109c65ecb73b9e
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-09-01 11:05:38 +0530

    Add auth v1

---
+++ BlogPost_Models/Data/DTOs/AuthDTO/LoginResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost_Models.Data.DTOs.AuthDTO
+{
+    public class LoginResponseDTO
+    {
+        [Required(ErrorMessage = "Token is required.")]
+        public string Token { get; set; }
+
+        [Required(ErrorMessage = "Role is required.")]
+        public string Role { get; set; }
+    }
+}
