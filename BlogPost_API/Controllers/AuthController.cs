a56896ac3e1a6abb5cd5b03dd6b55075198e0581
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-09-03 01:29:37 +0530

    Configure auth Controller

--- BlogPost_API/Controllers/AuthController.cs
+++ BlogPost_API/Controllers/AuthController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlogPost_Models.Data.DTOs.AuthDTO;
+using Blogpost_Service.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogPost_API.Controllers
@@ -7,5 +11,65 @@ namespace BlogPost_API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthService _service;
+
+        public AuthController(IAuthService service)
+        {
+            _service = service;
+        }
+
+
+        [Authorize]
+        [HttpPost("LoginUser")]
+        public async Task<IActionResult> Login(LoginRequestDTO request)
+        {
+            var result = await _service.LoginAsyncService(request);
+
+
+            if (result == null)
+            {
+                return Unauthorized(new
+                {
+                    status = 401,
+                    message = "Invalid Username and password"
+                });
+            }
+
+            else
+            {
+                return Ok(new
+                {
+                    status = 200,
+                    message = "Login Successfully",
+                    data = result
+                });
+            }
+        }
+
+        [Authorize]
+        [HttpPost("ResisterUser")]
+        public async Task<IActionResult> Register(RegisterRequestDTO request)
+        {
+            var result = await _service.RegisterAsyncService(request);
+
+            if (result)
+            {
+                return Ok(new
+                {
+                    status = 200,
+                    message = "Register Successfully"
+                });
+            }
+            else
+            {
+                return Conflict(new
+                {
+                    status = 409,
+                    message = "Username Already Exist"
+                });
+            }
+        }
+
+
     }
 }
