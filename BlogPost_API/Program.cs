7673b206706ce533ff1a11300427984d26faf82c
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-09-03 00:48:13 +0530

    Configure Program.cs

--- BlogPost_API/Program.cs
+++ BlogPost_API/Program.cs
@@ -1,10 +1,14 @@
-using BlogPost_API.Data;
+﻿using BlogPost_API.Data;
 using Blogpost_DataAccess.Interface;
 using Blogpost_DataAccess.Repositary;
 using Blogpost_Service.Interface;
 using Blogpost_Service.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -40,6 +44,64 @@ builder.Services.AddScoped<IBlogpostService, BlogpostService>();
 builder.Services.AddScoped<IImageRepo, ImageRepository>();
 builder.Services.AddScoped<IImageRepo, ImageRepository>();
 builder.Services.AddScoped<IImageService, ImageService>();
+
+
+
+//-------------JWT AUTHENTICATION FOR SWAGGER --------------------
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ContactAppNLayer.Api", Version = "v1" });
+
+    // 🔐 JWT Authentication Support
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' followed by space and JWT token. Example: Bearer eyJhbGciOi..."
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+
+
+// ---------------- JWT Auth -----------------------
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])
+            ),
+            ClockSkew = TimeSpan.Zero // <-- No buffer; strict 15-min expiry
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 
 var app = builder.Build();
 
